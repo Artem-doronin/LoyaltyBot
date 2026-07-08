7ff30ecb81e23656cec19bb3530ac0700430243a
Artem Doronin <doronintema2018@yandex.ru>
2026-07-08 10:11:40 +0500

    Initial commit

---
+++ src/main/java/com/example/LoyaltyBot/LoyaltyBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.LoyaltyBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LoyaltyBot/LoyaltyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LoyaltyBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
